ba553721be8d0bf3bca10908d5e02e9b250bc2e2
Harshitha Yasiru <90701403+yassa-life@users.noreply.github.com>
2025-07-13 15:44:15 +0530

    Update attendance.sql

--- attendance.sql
+++ attendance.sql
@@ -1,21 +1,30 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.30 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             12.8.0.6908
--- --------------------------------------------------------
+CREATE DATABASE  IF NOT EXISTS `attendance` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `attendance`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: attendance
+-- ------------------------------------------------------
+-- Server version	8.0.30
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping structure for table attendance.administration
-CREATE TABLE IF NOT EXISTS `administration` (
+--
+-- Table structure for table `administration`
+--
+
+DROP TABLE IF EXISTS `administration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `administration` (
   `admin_id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `user_id` int NOT NULL,
@@ -24,11 +33,16 @@ CREATE TABLE IF NOT EXISTS `administration` (
   KEY `FK_administration_users` (`user_id`),
   CONSTRAINT `FK_administration_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Table structure for table `attendance`
+--
 
--- Dumping structure for table attendance.attendance
-CREATE TABLE IF NOT EXISTS `attendance` (
+DROP TABLE IF EXISTS `attendance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `attendance` (
   `attendance_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int DEFAULT NULL,
   `method` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
@@ -37,12 +51,17 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   PRIMARY KEY (`attendance_id`),
   KEY `FK_attendance_users` (`user_id`),
   CONSTRAINT `FK_attendance_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Table structure for table `leave_application`
+--
 
--- Dumping structure for table attendance.leave_application
-CREATE TABLE IF NOT EXISTS `leave_application` (
+DROP TABLE IF EXISTS `leave_application`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `leave_application` (
   `leave_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `leave_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -57,11 +76,16 @@ CREATE TABLE IF NOT EXISTS `leave_application` (
   CONSTRAINT `FK_leave_application_administration` FOREIGN KEY (`approevs_by`) REFERENCES `administration` (`admin_id`),
   CONSTRAINT `FK_leave_application_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Table structure for table `rfid`
+--
 
--- Dumping structure for table attendance.rfid
-CREATE TABLE IF NOT EXISTS `rfid` (
+DROP TABLE IF EXISTS `rfid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rfid` (
   `card_id` varchar(50) NOT NULL DEFAULT 'AUTO_INCREMENT',
   `user_id` int NOT NULL,
   `is_active` tinyint(1) DEFAULT '1',
@@ -71,11 +95,16 @@ CREATE TABLE IF NOT EXISTS `rfid` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `rfid_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Table structure for table `temp_leave`
+--
 
--- Dumping structure for table attendance.temp_leave
-CREATE TABLE IF NOT EXISTS `temp_leave` (
+DROP TABLE IF EXISTS `temp_leave`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `temp_leave` (
   `record_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL DEFAULT (0),
   `method` varchar(50) DEFAULT 'faceid',
@@ -85,12 +114,37 @@ CREATE TABLE IF NOT EXISTS `temp_leave` (
   KEY `user_id` (`user_id`),
   KEY `timestamp` (`timestamp`),
   CONSTRAINT `FK_temp_leave_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Data exporting was unselected.
+--
+-- Table structure for table `user_lo`
+--
 
--- Dumping structure for table attendance.users
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `user_lo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_lo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pwd` varchar(50) NOT NULL,
+  `rst_pwd` varchar(50) DEFAULT NULL,
+  `rst_pwd_expiry` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_user_lo_users` (`user_id`),
+  CONSTRAINT `FK_user_lo_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
@@ -104,26 +158,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- Data exporting was unselected.
-
--- Dumping structure for table attendance.user_lo
-CREATE TABLE IF NOT EXISTS `user_lo` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `pwd` varchar(50) NOT NULL,
-  `rst_pwd` varchar(50) DEFAULT NULL,
-  `rst_pwd_expiry` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_user_lo_users` (`user_id`),
-  CONSTRAINT `FK_user_lo_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-13 15:43:47
